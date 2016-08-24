6bc5e6de285ea65fea413757c6cef1a1432c5f79
Bor Grošelj Simić <bor.groseljsimic@telemach.net>
2016-08-24 13:52:43 +0200

    modified to fit the removal of Token class

--- src/core.py
+++ src/core.py
@@ -1,47 +1,53 @@
-import tiles
+from tile import Tile
 from constants import *
 
 class Core:
     size = TABLESIZE
     def __init__(self):
-        self.grid = [[tiles.Square(i, j) for i in range(self.size)]
+        self.grid = [[Tile(i, j) for i in range(self.size)]
                      for j in range(self.size)]
         self.token_to_move = None
     
     def get_token_pos(self, x, y):
-        if tiles.Square(x, y).is_special:
-            print("im special", x, y)
+        if Tile(x, y).is_special:
             return (x, y)
         where_to_move = self.grid[x][y].where_is()
         tmp_x = x
         tmp_y = y        
         if where_to_move[0] == -1:
-            while (self.grid[tmp_x][tmp_y].inhabitor == None and
+            while (self.grid[tmp_x][tmp_y].sign == 0 and
                    tmp_x + 1):
                 tmp_x -= 1
             return (tmp_x + 1, tmp_y)
-        if where_to_move[0] == 1:
-            while (self.grid[tmp_x][tmp_y].inhabitor == None and
-                   tmp_x + 2< self.size):
-                tmp_x += 1
-            return (tmp_x - 1, tmp_y)
+
         if where_to_move[1] == -1:
-            while (self.grid[tmp_x][tmp_y].inhabitor == None and
+            while (self.grid[tmp_x][tmp_y].sign == 0 and
                    tmp_y + 1):
                 tmp_y -= 1
             return (tmp_x, tmp_y + 1)
+
+        if where_to_move[0] == 1:
+            while (self.grid[tmp_x][tmp_y].sign == 0 and
+                   tmp_x + 1< self.size):
+                tmp_x += 1
+            if tmp_x + 1 < self.size:
+                return (tmp_x - 1, tmp_y)
+            else:
+                return (tmp_x, tmp_y)
+
         if where_to_move[1] == 1:
-            while (self.grid[tmp_x][tmp_y].inhabitor == None and
+            while (self.grid[tmp_x][tmp_y].sign == 0 and
                    tmp_y + 2 < self.size):
                 tmp_y += 1
-            return (tmp_x, tmp_y - 1)
+            if tmp_x + 1 < self.size:
+                return (tmp_x - 1, tmp_y)
+            else:
+                return (tmp_x, tmp_y)
+
 
     def insert_token(self, x, y, sign):
-       # assert (abs(x + y - self.size) == 1 or x == y + 1 or x + 1 == y or)
         token_pos = self.get_token_pos(x, y)
-        self.grid[token_pos[0]][token_pos[1]].inhabitor = tiles.Token(
-            token_pos[0], token_pos[1], sign)
-
+        self.grid[token_pos[0]][token_pos[1]].sign = sign
 
     def end(self):
         return False
