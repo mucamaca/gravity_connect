b44103a5ad50d9940e25fc0493d459079d32b1bf
jakobzmrzlikar <jakob.zmrzlikar@gmail.com>
2016-08-24 11:32:59 +0200

    something is still wrong with drop_token()

--- src/GUI.py
+++ src/GUI.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-from constants import * 
+from constants import *
+from time import sleep
 
 class GUI:
     def __init__(self, core):
@@ -7,10 +8,10 @@ class GUI:
         self.height, self.width = FRAMESIZE, FRAMESIZE
         self.grid = self.height / 10
         self.root = tk.Tk()
-        
         self.turn = True
         
         self.make_canvas()
+
         self.root.mainloop()
 
     def make_canvas(self):
@@ -18,60 +19,62 @@ class GUI:
         self.map.bind('<Button-1>', self.mouse_click)
         self.map.grid(row=0, column=0)
 
-        self.load_map(self.core.grid, self.map) 
+        self.load_map() 
 
     def mouse_click(self, event):
-        if not self.core.end():
-            mouse_x = int(event.x // self.grid) 
-            mouse_y = int(event.y // self.grid)
+        if self.core.end():
+            return
+        mouse_x = int(event.x // self.grid) 
+        mouse_y = int(event.y // self.grid)
+        
+        if self.valid_coords(mouse_x, mouse_y):
+            self.drop_token(mouse_x, mouse_y)
+            sign = (not self.turn) + 1
+            self.core.insert_token(mouse_x, mouse_y, sign)
+            self.turn = not self.turn
             
-            if self.valid_coords(mouse_x, mouse_y):
-                self.drop_token(mouse_x, mouse_y)
-                sign = (not self.turn) + 1
-                self.core.insert_token(mouse_x, mouse_y, sign)
-                self.turn = not self.turn
-                
-                self.load_map(self.core.grid, self.map)
+            #self.load_map()
 
-    def load_map(self, grid, map):
-        self.map = map
+    def load_map(self):
         self.map.delete('all')
         # Dhonraws the grid
-        for i in range(len(grid)):
+        for i in range(TABLESIZE):
             self.map.create_line(0, i * self.grid, TABLESIZE * self.grid, i * self.grid)
             self.map.create_line(i * self.grid, 0, i * self.grid, TABLESIZE * self.grid)
 
         # Draws the tokens and special fields:
-        for i in range(len(grid)):
-            for j in range(len(grid)):
-                if grid[i][j].sign == 0:
-                    pass
-                elif grid[i][j].sign == 1:
+        for i in range(TABLESIZE):
+            for j in range(TABLESIZE):
+                if self.core.grid[i][j].sign == 0:
+                    colour = "white"
+                elif self.core.grid[i][j].sign == 1:
                     colour = "blue"
-                elif grid[i][j].sign == 2:
+                elif self.core.grid[i][j].sign == 2:
                     colour = "red"
                     
-                if grid[i][j].is_special:
+                if self.core.grid[i][j].is_special:
                     colour = "gray"
-                    self.map.create_rectangle(i * self.grid, j * self.grid,
-                                              (i + 1) * self.grid, (j + 1) * self.grid, fill=colour)
+                self.map.create_rectangle(i * self.grid, j * self.grid,
+                                        (i + 1) * self.grid, (j + 1) * self.grid, fill=colour)
 
     def drop_token(self, x, y):
         pos = self.core.get_token_pos(x, y)
         move_dir = self.core.grid[x][y].where_is()
-        print("x: ", x, "y: ", y)
-        print("pos ", pos)
-        print("move_dir ", move_dir)
-        while (x != pos[0]) and (y != pos[1]):
+        while (x != pos[0]) or (y != pos[1]):
             if self.turn:
                 colour = "blue"
             else:
                 colour = "red"
-                
+            
+            self.load_map() 
             self.map.create_rectangle(x * self.grid, y * self.grid,
-                                      (x + 1) * self.grid, (y + 1) * self.grid, fill=colour)
+                                      (x+1) * self.grid, (y+1) * self.grid, fill="red")
+
             x += move_dir[0]
             y += move_dir[1]
+
+            #self.map.delete("all")
+
                 
     def valid_coords(self, x, y):
         return True
