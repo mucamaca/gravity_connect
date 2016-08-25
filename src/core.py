686b204362222e7e9512cc7fa4cbe1e59b23e5ee
Bor Grošelj Simić <bor.groseljsimic@telemach.net>
2016-08-25 15:38:31 +0200

    this is a commit

--- src/core.py
+++ src/core.py
@@ -48,41 +48,42 @@ class Core:
     def insert_token(self, x, y, sign):
         token_pos = self.get_token_pos(x, y)
         self.grid[token_pos[0]][token_pos[1]].sign = sign
+        return token_pos
 
+    def get_tuples(self, i, j, k ,l):
+        if i == k:
+            return list(zip([i] * abs(j - l), range(j, l)))
+        elif j == l:
+            return list(zip(range(i, k), [j] * abs(i - k)))
+        elif j > l:
+            return list(zip(range(i, k), range(j, l, -1)))
+        else:
+            return list(zip(range(i, k), range(j, l)))
+
+    def is_4inarow(self, i, j, k, l, sign):
+        for z in self.get_tuples(i, j, k, l):
+            if self.grid[z[0]][z[1]].sign != sign:
+                return False
+        return True
+            
+        
     def end(self, x ,y):
         c_sign = self.grid[x][y].sign
-        
+        ret_val = True
         for i in range(x - 3, x + 1):
             if(i >= 0 and i + 3 < TABLESIZE):
-                for j in range(i, i + 4):
-                    if self.grid[j][y].sign != c_sign:
-                        return False
+               ret_val = self.is_4inarow(i, y, i + 4, y, c_sign)
+                        
         for i in range(y - 3, y + 1):
             if(i >= 0 and i + 3 < TABLESIZE):
-                for j in range(i, i + 4):
-                    if self.grid[x][j].sign != c_sign:
-                        return False
+                ret_val = self.is_4inarow(x, i, x, i + 4, c_sign)
+
         for i, j in zip(range(x - 3, x + 1), range(y - 3, y + 1)):
             if i >= 0 and j >=0 and i + 3 < TABLESIZE and j + 3 < TABLESIZE:
-                for k, l in zip(range(i, i + 4), range(j, j + 4)):
-                    if self.grid[k][l].sign != c_sign:
-                        return False
+                ret_val = self.is_4inarow(i, j, i + 4, j + 4, c_sign)
+                
         for i, j in zip(range(x - 3, x + 1), range(y, y - 4)):
             if i >= 0 and j - 3 >= 0 and j < TABLESIZE and i + 3 < TABLESIZE:
-                for k, l in zip(range(i, i + 4), range(j, j - 4)):
-                    if self.grid[k][l].sign != c_sign:
-                        return False
-        return True
-        
-        
-    
+                ret_val = self.is_4inarow(i, j, i + 4, j - 4, c_sign)
 
-    
-        
-            
-            
-                
-            
-        
-        
-        
+        return ret_val
