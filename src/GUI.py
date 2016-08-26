9388c7907211a0d9ffc473bf3c4d28387f6a0780
Bor Grošelj Simić <bor.groseljsimic@telemach.net>
2016-08-26 10:01:26 +0200

    meow

--- src/GUI.py
+++ src/GUI.py
@@ -27,27 +27,17 @@ class GUI:
         mouse_y = int(event.y // self.grid)
 
         if self.valid_coords(mouse_x, mouse_y):
-<<<<<<< HEAD
             self.drop_token(mouse_x, mouse_y, self.core.get_token_pos(mouse_x, mouse_y))
-            self.root.after(self.increment_id(mouse_x, mouse_y) * 250, self.place_token, mouse_x, mouse_y)
+            self.root.after(self.increment_id(mouse_x, mouse_y) * 250,
+                            self.place_token, mouse_x, mouse_y)
 
     def place_token(self, x, y):
         sign = (not self.turn) + 1
-        self.core.insert_token(x, y, sign)
-
-        if self.core.end(*self.core.get_token_pos(x, y)):
+        if self.core.insert_token(x, y, sign):
             exit(0)
         self.turn = not self.turn
         self.load_map()
-=======
-            self.drop_token(mouse_x, mouse_y)
-            sign = (not self.turn) + 1
-            if self.core.insert_token(mouse_x, mouse_y, sign):
-                print("ovojemaca")
-                exit(0)
-            self.turn = not self.turn
-            self.load_map()
->>>>>>> rewrite_win
+
 
     def load_map(self):
         self.map.delete('all')
@@ -81,23 +71,15 @@ class GUI:
             self.load_map()
             self.map.create_rectangle(x * self.grid, y * self.grid,
                                       (x+1) * self.grid, (y+1) * self.grid, fill=colour)
-<<<<<<< HEAD
-            
-=======
-            self.root.after(50, do_nothing())
-
->>>>>>> rewrite_win
             x += move_dir[0]
             y += move_dir[1]
 
             self.root.after(250, self.drop_token, x, y, pos)
             print("dopping")
-        else:
-            pass
 
     def valid_coords(self, x, y):
         return not self.core.grid[x][y].sign
-<<<<<<< HEAD
+
 
     def increment_id(self, x, y):
         pos = self.core.get_token_pos(x, y)
@@ -108,8 +90,5 @@ class GUI:
         self.switch_id = diff
         return self.switch_id
 
-=======
-                 
->>>>>>> rewrite_win
 def do_nothing():
     pass
