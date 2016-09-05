5cf5b8d63bcb0228cb98bc3a073c2c4ad7d9464e
Bor Grošelj Simić <bor.groseljsimic@telemach.net>
2016-09-05 10:26:51 +0200

    removed quite a few bugs

--- src/ai.py
+++ src/ai.py
@@ -1,26 +1,27 @@
 from score import score
 
 
-def minimax(core, turn, depth):
-    sign = not turn + 1
+def minimax(core, turn, depth, x, y):
+    sign = (not turn) + 1
     if not depth:
-        print("score!!!")
-        return score(core, sign)
+        return score(core, sign), x, y
     if turn:
         best = 1000000000000
         for i, j in core.valid_list:
+            i, j = core.get_token_pos(i, j)
             core.grid[i][j] = sign
-            v = minimax(core, not turn, depth - 1)
+            v = minimax(core, not turn, depth - 1, i, j)
             core.grid[i][j] = 0
-            if best < v:
-                best = v
-        return best
+            if best > v[0]:
+                best, x, y = v 
+        return best, x, y
     else:
         best = -1000000000000
         for i, j in core.valid_list:
+            i, j = core.get_token_pos(i, j)
             core.grid[i][j] = sign
-            v = minimax(core, not turn, depth - 1)
+            v = minimax(core, not turn, depth - 1, i, j)
             core.grid[i][j] = 0
-            if best > v:
-                best = v
-        return best
+            if best < v[0]:
+                best, x, y = v
+        return best, x, y
