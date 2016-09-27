a5886b7bf6a046ba4b53b71e95ed98001ab8bf32
Bor Grošelj Simić <bor.groseljsimic@telemach.net>
2016-09-27 08:48:15 +0200

    removed the unneeded sign variable

--- src/ai.py
+++ src/ai.py
@@ -1,27 +1,24 @@
-from score import score
-
-
 def minimax(core, turn, depth, x, y):
-    sign = (not turn) + 1
     if not depth:
-        return score(core, sign), x, y
+        m = core.score(), x, y
+        return m
     if turn:
-        best = 1000000000000
-        for i, j in core.valid_list:
-            i, j = core.get_token_pos(i, j)
-            core.grid[i][j] = sign
-            v = minimax(core, not turn, depth - 1, i, j)
-            core.grid[i][j] = 0
-            if best > v[0]:
-                best, x, y = v 
-        return best, x, y
-    else:
         best = -1000000000000
         for i, j in core.valid_list:
             i, j = core.get_token_pos(i, j)
-            core.grid[i][j] = sign
+            core.grid[i][j] = turn + 1
             v = minimax(core, not turn, depth - 1, i, j)
             core.grid[i][j] = 0
             if best < v[0]:
+                best, x, y = v 
+        return best, x, y
+    else:
+        best = 1000000000000
+        for i, j in core.valid_list:
+            i, j = core.get_token_pos(i, j)
+            core.grid[i][j] = turn + 1
+            v = minimax(core, not turn, depth - 1, i, j)
+            core.grid[i][j] = 0
+            if best > v[0]:
                 best, x, y = v
         return best, x, y
