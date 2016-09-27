284a50e12b6696f1807a0f5c41bc2dfb53cf4a85
Bor Grošelj Simić <bor.groseljsimic@telemach.net>
2016-09-27 08:45:18 +0200

    merged score.py file into Core class

--- src/core.py
+++ src/core.py
@@ -2,6 +2,7 @@ from constants import *
 
 class Core:
     size = TABLESIZE
+    score_list = [0, 16, 400, 1800, 100000]
     def __init__(self):
         self.valid_list = []
         for i, j in zip(range(self.size),range(self.size)):
@@ -179,3 +180,127 @@ class Core:
             return c_sign
 
         return False
+
+    
+    def score(self):
+        scor = 0
+        for i,j in self.valid_list:
+            chg = self.where_is(i, j)
+            pos = list(self.get_token_pos(i, j))
+            if (pos[0] != 0 and pos[1] != 0 and
+                pos[0] != TABLESIZE - 1 and pos[1] != TABLESIZE - 1):
+                pos[0] += chg[0]
+                pos[1] += chg[1]
+            for t in self.list_of_tuples(*pos):
+                scor += self.tup_score(t)
+            if chg != (0, 0):
+                while (pos[0] != 0 and pos[1] != 0 and
+                       pos[0] != TABLESIZE - 1 and pos[1] != TABLESIZE - 1):
+                    pos[0] += chg[0]
+                    pos[1] += chg[1]
+                    for t in self.list_of_tuples(*pos):
+                        scor += self.tup_score(t)
+        return scor
+
+
+    def count(self, tup, sign):
+        _count = 0
+        for i, j in tup:
+            if self.grid[i][j] == sign:
+                _count += 1
+        return _count
+
+
+    def tup_score(self, tup):
+        if self.count(tup, CENTER_SIGN):
+            return 0
+        computer = self.count(tup, COMPUTER_SIGN)
+        player = self.count(tup, PLAYER_SIGN)
+        if computer and player:
+            return 0
+        elif player:
+            return -(self.score_list[player]) // (self.get_req_moves(tup) ** 2 + 1)
+        elif computer:
+            return self.score_list[computer] // (self.get_req_moves(tup) ** 2 + 1)
+        else:
+            return 0
+
+    def list_of_tuples(self, x, y):
+        ret_val = []
+        for i in range(x - 3, x + 1):
+            if i >= 0 and i + 3 < TABLESIZE:
+                ret_val.append(self.get_tuples(i, y, i + 4, y))
+
+        for i in range(y - 3, y + 1):
+            if i >= 0 and i + 3 < TABLESIZE:
+                ret_val.append(self.get_tuples(x, i, x, i + 4))
+
+        for i, j in zip(range(x - 3, x + 1), range(y - 3, y + 1)):
+            if i >= 0 and j >=0 and i + 3 < TABLESIZE and j + 3 < TABLESIZE:
+                ret_val.append(self.get_tuples(i, j, i + 4, j + 4))
+
+        for i, j in zip(range(x - 3, x + 1), range(y, y - 4)):
+            if i >= 0 and j - 3 >= 0 and j < TABLESIZE and i + 3 < TABLESIZE:
+                ret_val.append(self.get_tuples(i, j, i + 4, j - 4))
+        return ret_val
+
+    
+    def get_tuples(self, i, j, k ,l):
+        if i == k:
+            return list(zip([i] * abs(j - l), range(j, l)))
+        elif j == l:
+            return list(zip(range(i, k), [j] * abs(i - k)))
+        elif j > l:
+            return list(zip(range(i, k), range(j, l, -1)))
+        else:
+            return list(zip(range(i, k), range(j, l)))
+
+
+    
+    # can be much optimized
+    def get_req_moves(self, tup):
+        sm = val = 0
+        for x,y in tup:
+            if self.is_special(x, y):
+                continue
+            where_to_move = self.where_is(x, y)
+            tmp_x = x
+            tmp_y = y
+            val = 0
+            if where_to_move[0] == -1:
+                while (self.grid[tmp_x][tmp_y] == 0 and
+                       tmp_x + 1):
+                    tmp_x -= 1
+                    val += 1
+                sm+=val -1
+                continue
+            if where_to_move[1] == -1:
+                while (self.grid[tmp_x][tmp_y] == 0 and
+                       tmp_y + 1):
+                    tmp_y -= 1
+                    val += 1
+                sm+=val - 1
+                continue
+
+            if where_to_move[0] == 1:
+                while (self.grid[tmp_x][tmp_y] == 0 and
+                       tmp_x + 1 < self.size):
+                    tmp_x += 1
+                    val += 1
+                if tmp_x + 1 < self.size and self.grid[tmp_x][tmp_y] != 0:
+                    sm+= val -1                    
+                else:
+                    sm+=val
+                continue
+
+            if where_to_move[1] == 1:
+                while (self.grid[tmp_x][tmp_y] == 0 and
+                       tmp_y + 1 < self.size):
+                    tmp_y += 1
+                    val += 1
+                if tmp_y + 1 < self.size and self.grid[tmp_x][tmp_y] != 0:
+                    sm+=val-1
+                else:
+                    sm+=val
+        return sm
+
