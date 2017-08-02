50f3c788171e05cf139ed29bd4ebb0019fcd7957
Bor Grošelj Simić <bor.groseljsimic@telemach.net>
2017-08-02 12:31:20 +0200

    fixed core.py's broken indentation

--- src/core.py
+++ src/core.py
@@ -1,20 +1,13 @@
-    from constants import *
-    from grid import Grid
-    from shape_loader import load_shape
+from constants import *
+from grid import Grid
+from shape_loader import load_shape
 
 
-    class Core:
-        size = TABLESIZE
-        score_list = [0, 16, 400, 1800, 100000]
-        def __init__(self, game_options=GameOptions()):
-
-            self.num_of_players = game_options.num_of_players
-            self.ai_players = game_options.ai_players
-            self.grid = Grid(gametype)
-            self.turn = 0
-
-       ` 
-
-
-
-
+class Core:
+    size = TABLESIZE
+    score_list = [0, 16, 400, 1800, 100000]
+    def __init__(self, game_options=GameOptions()):
+        self.num_of_players = game_options.num_of_players
+        self.ai_players = game_options.ai_players
+        self.grid = Grid(gametype)
+        self.turn = 0
