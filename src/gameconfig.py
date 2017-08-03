ce132bf993daee880c8d50400ee16dc303f355eb
Bor Grošelj Simić <bor.groseljsimic@telemach.net>
2017-08-03 17:51:02 +0200

    added a new class for handling game configurations

---
+++ src/gameconfig.py
@@ -0,0 +1,23 @@
+""" This module defines the type for storing game configuration. """
+
+class GameConfig:
+    num_of_players = 2
+    ai_players = [None, None]
+    x_size = 10
+    y_size = 10
+    
+    game_type = 0
+
+    _dir_list = [(-1, -1), (-1, 0, (-1, 1), (0, -1), (0, 0), 
+        (0, 1), (1, -1), (1, 0), (1, 1), None]
+    
+    def load_shape(self):
+        shape = []
+        with open(str(game_type)+'.cfg') as conf:
+            for line in conf:
+                shape.append([])
+                for c in line:
+                    shape[-1].append(self._dir_list[c])
+        self.x_size = len(shape[0])
+        self.y_size = len(shape)
+        return shape
