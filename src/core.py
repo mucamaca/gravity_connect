811239d1499a216b0657394a505f4da27782e92d
Bor Grošelj Simić <bor.groseljsimic@telemach.net>
2017-08-04 12:40:15 +0200

    Hopefully fixed the issue #7 on github

--- src/core.py
+++ src/core.py
@@ -7,7 +7,19 @@ class Core:
         self.grid = Grid()
         self.turn = 0
 
+    def can_insert(self, x, y):
+        """ Returns True if it is possible to put a token on self.grid[x][y],
+        otherwise returns False
+        """
+        if(self.grid[x][y].state == Tile.EMPTY):
+            return True
+        return False
+
+
     def insert_token(self, x, y):
+        """ Checks where a token would fall when put in grid[x][y] 
+        and inserts it there. Returns True on success, False otherwise
+        """
         if(self.grid[x][y].state != Tile.EMPTY):
             return False
         update_tile(x, y, self.turn)
