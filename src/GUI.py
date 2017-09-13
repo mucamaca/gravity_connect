aba84c17c5284507a4e64c21aaeee2bc81efc329
Bor <bor.groseljsimic@telemach.net>
2017-09-13 13:42:46 +0200

    some refactoring of fallin animation code

--- src/GUI.py
+++ src/GUI.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import tkinter as tk
-from copy import deepcopy
+from copy import copy, deepcopy
 
 from tile import Tile
 from core import Core
@@ -21,7 +21,7 @@ class GUI:
     c = 1
     def __init__(self):
         self.core = Core()
-        self.grid = deepcopy(self.core.grid)
+        self.state_grid =  self.core.get_state_grid()
         self.height, self.width = 400, 400
         self.tablesize = 10
         self.cellsize = 40
@@ -60,7 +60,7 @@ class GUI:
             # Draws the tokens and special fields:
             for i in range(self.tablesize):
                 for j in range(self.tablesize):
-                    if self.grid[i][j].state not in Tile.PLAYERS:
+                    if self.state_grid[i][j] not in Tile.PLAYERS:
                         self.map.create_rectangle(
                             i * self.cellsize, j * self.cellsize,
                             (i + 1) * self.cellsize,
@@ -69,7 +69,7 @@ class GUI:
                     self.map.create_rectangle(
                             i * self.cellsize, j * self.cellsize,
                             (i + 1) * self.cellsize,
-                            (j + 1) * self.cellsize, fill=self.colour_list[self.grid[i][j].state])
+                            (j + 1) * self.cellsize, fill=self.colour_list[self.state_grid[i][j]])
 
 
     def mouse_click(self, event):
@@ -79,45 +79,35 @@ class GUI:
         if self.core.can_insert(mouse_x, mouse_y) and not self.lock_click:
             self.place_token(mouse_x, mouse_y)
 
-    def drop_token(self, x, y, pos):
-        move_dir = self.grid[x][y].dir
+    def drop_token(self, x, y):
+        move_dir = self.core.get_dir(x, y)
 
         # recursively drops the token
-        if (x, y) != pos:
-            colour = self.colour_list[self.turn]
-
-            self.load_map()
-            self.map.create_rectangle(
-                x * self.cellsize, y * self.cellsize,
-                (x+1) * self.cellsize, (y+1) * self.cellsize, fill=colour
-            )
-
-            x += move_dir[0]
-            y += move_dir[1]
-
-            self.root.after(250, self.drop_token, x, y, pos)
-        else:
-            if self.core.insert_token(*pos) != -1:
+        if (move_dir) == (0, 0):
+            if self.core.insert_token(x, y) != -1:
                 self.game_end()
-            self.grid = deepcopy(self.core.grid)
+            self.state_grid = self.core.get_state_grid()
             self.load_map()
             self.turn = (self.turn + 1) % config.num_of_players
             self.lock_click = False
+            return
 
+        colour = self.colour_list[self.turn]
 
-    # This code is probably not needed anymore
-    # def increment_id(self, x, y):
-    #     pos = self.grid.get_token_pos(x, y)
-    #     if x == pos[0]:
-    #         return abs(y - pos[1])
-    #     else:
-    #         return abs(x - pos[0])
+        self.load_map()
+        self.map.create_rectangle(
+            x * self.cellsize, y * self.cellsize,
+            (x+1) * self.cellsize, (y+1) * self.cellsize, fill=colour
+        )
+
+        x += move_dir[0]
+        y += move_dir[1]
+
+        self.root.after(250, self.drop_token, x, y)
 
     def place_token(self, x, y):
-        pos = self.grid.get_token_pos(x, y)
-        pos = (pos[0], pos[1])
         self.lock_click = True
-        self.drop_token(x, y, pos)
+        self.drop_token(x, y)
 
     def game_end(self):
         self.end_screen = tk.Tk()
